c9c45e1f6515a72db2900053192b8e5c86a05fe4
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-15 18:54:16 +0600

    banner added

---
+++ src/components/Carousel/Carousel.js
@@ -0,0 +1,88 @@
+import React from 'react';
+
+const Carousel = () => {
+    return (
+        <div>
+            <div id="carouselExampleCaptions" className="carousel slide relative" data-bs-ride="carousel">
+                <div className="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide-to="0"
+                        className="active"
+                        aria-current="true"
+                        aria-label="Slide 1"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide-to="1"
+                        aria-label="Slide 2"
+                    ></button>
+                    <button
+                        type="button"
+                        data-bs-target="#carouselExampleCaptions"
+                        data-bs-slide-to="2"
+                        aria-label="Slide 3"
+                    ></button>
+                </div>
+                <div className="carousel-inner relative w-full overflow-hidden">
+                    <div className="carousel-item active relative float-left w-full">
+                        <img
+                            src="https://i.ibb.co/HrQ2gf7/slider-3.png"
+                            className="block w-full"
+                            alt="..."
+                        />
+                        <div className="carousel-caption hidden md:block absolute text-center">
+                            <h5 className="text-xl">Big capacity</h5>
+                            <p>We have big place to store products</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item relative float-left w-full">
+                        <img
+                            src="https://i.ibb.co/w4hWf9q/slider-2.png"
+                            className="block w-full"
+                            alt="..."
+                        />
+                        <div className="carousel-caption hidden md:block absolute text-center">
+                            <h5 className="text-xl">Best services</h5>
+                            <p>We give best services</p>
+                        </div>
+                    </div>
+                    <div className="carousel-item relative float-left w-full">
+                        <img
+
+                            src="https://i.ibb.co/1KhsDZt/slider-1.png"
+                            className="block w-full"
+                            alt="..."
+                        />
+                        <div className="carousel-caption hidden md:block absolute text-center">
+                            <h5 className="text-xl">Qualified employee</h5>
+                            <p>Our all employers are ver qualified</p>
+                        </div>
+                    </div>
+                </div>
+                <button
+                    className="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="prev"
+                >
+                    <span className="carousel-control-prev-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Previous</span>
+                </button>
+                <button
+                    className="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
+                    type="button"
+                    data-bs-target="#carouselExampleCaptions"
+                    data-bs-slide="next"
+                >
+                    <span className="carousel-control-next-icon inline-block bg-no-repeat" aria-hidden="true"></span>
+                    <span className="visually-hidden">Next</span>
+                </button>
+            </div>
+        </div>
+    );
+};
+
+export default Carousel;
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import Carousel from '../Carousel/Carousel';
 
 const Header = () => {
     return (
-        <nav className="
-  relative
+        <div>
+            <nav className="
+  sticky-top
   w-full
   flex flex-wrap
   items-center
@@ -17,8 +19,8 @@ const Header = () => {
   shadow-lg
   navbar navbar-expand-lg navbar-light
   ">
-            <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
-                <button className="
+                <div className="container-fluid w-full flex flex-wrap items-center justify-between px-6">
+                    <button className="
       navbar-toggler
       text-gray-500
       border-0
@@ -28,16 +30,16 @@ const Header = () => {
       bg-transparent
       focus:outline-none focus:ring-0 focus:shadow-none focus:no-underline
     " type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                    aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
-                        className="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-                        <path fill="currentColor"
-                            d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
-                        </path>
-                    </svg>
-                </button>
-                <div className="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
-                    <Link className="
+                        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <svg aria-hidden="true" focusable="false" data-prefix="fas" data-icon="bars"
+                            className="w-6" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
+                            <path fill="currentColor"
+                                d="M16 132h416c8.837 0 16-7.163 16-16V76c0-8.837-7.163-16-16-16H16C7.163 60 0 67.163 0 76v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16zm0 160h416c8.837 0 16-7.163 16-16v-40c0-8.837-7.163-16-16-16H16c-8.837 0-16 7.163-16 16v40c0 8.837 7.163 16 16 16z">
+                            </path>
+                        </svg>
+                    </button>
+                    <div className="collapse navbar-collapse flex-grow items-center" id="navbarSupportedContent">
+                        <Link className="
         flex
         items-center
         text-gray-900
@@ -47,34 +49,34 @@ const Header = () => {
         lg:mt-0
         mr-1
       " to="#">
-                        <img src="https://i.ibb.co/4R7Lppg/warehouse.png" style={{ height: '25px' }} alt=""
-                            loading="lazy" />
-                        <h1 className='font-bold ml-2'>Wholesale Warehouse</h1>
-                    </Link>
-
-                    <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
-                        <li className="nav-item p-2">
-                            <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="#">Dashboard</Link>
-                        </li>
-                        <li className="nav-item p-2">
-                            <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="#">About us</Link>
-                        </li>
-                        <li className="nav-item p-2">
-                            <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="#">Blogs</Link>
-                        </li>
-                    </ul>
-
-                </div>
-
-                <div className="flex items-center relative">
-
-                    <div className="dropdown relative">
-                        <Link className="dropdown-toggle flex items-center hidden-arrow" to="#" id="dropdownMenuButton2" role="button"
-                            data-bs-toggle="dropdown" aria-expanded="false">
-                            <img src="https://mdbootstrap.com/img/new/Linkvatars/2.jpg" className="rounded-full"
-                                style={{ height: '25px', width: '25px' }} alt="" loading="lazy" />
+                            <img src="https://i.ibb.co/4R7Lppg/warehouse.png" style={{ height: '25px' }} alt=""
+                                loading="lazy" />
+                            <h1 className='font-bold ml-2'>Smart Warehouse</h1>
                         </Link>
-                        <ul className="
+
+                        <ul className="navbar-nav flex flex-col pl-0 list-style-none mr-auto">
+                            <li className="nav-item p-2">
+                                <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="#">Dashboard</Link>
+                            </li>
+                            <li className="nav-item p-2">
+                                <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="#">About us</Link>
+                            </li>
+                            <li className="nav-item p-2">
+                                <Link className="nav-link text-gray-500 hover:text-gray-700 focus:text-gray-700 p-0" to="#">Blogs</Link>
+                            </li>
+                        </ul>
+
+                    </div>
+
+                    <div className="flex items-center relative bg-slate-400">
+
+                        <div className="dropdown relative">
+                            <Link className="dropdown-toggle flex items-center hidden-arrow" to="#" id="dropdownMenuButton2" role="button"
+                                data-bs-toggle="dropdown" aria-expanded="false">
+                                <img src="https://i.ibb.co/GndbNbD/avatar-ge95b7d95a-640.png" className="rounded-full bg-white"
+                                    style={{ height: '25px', width: '25px' }} alt="" loading="lazy" />
+                            </Link>
+                            <ul className="
     dropdown-menu
     min-w-max
     absolute
@@ -89,14 +91,13 @@ const Header = () => {
     shadow-lg
     mt-1
     hidden
-    m-0
     bg-clip-padding
     border-none
-    left-auto
     right-0
+   
   " aria-labelledby="dropdownMenuButton2">
-                            <li>
-                                <Link className="
+                                <li>
+                                    <Link className="
         dropdown-item
         text-sm
         py-2
@@ -109,9 +110,9 @@ const Header = () => {
         text-gray-700
         hover:bg-gray-100
       " to="#">My Items</Link>
-                            </li>
-                            <li>
-                                <Link className="
+                                </li>
+                                <li>
+                                    <Link className="
         dropdown-item
         text-sm
         py-2
@@ -124,9 +125,9 @@ const Header = () => {
         text-gray-700
         hover:bg-gray-100
       " to="#">Manage Item</Link>
-                            </li>
-                            <li>
-                                <Link className="
+                                </li>
+                                <li>
+                                    <Link className="
         dropdown-item
         text-sm
         py-2
@@ -139,9 +140,9 @@ const Header = () => {
         text-gray-700
         hover:bg-gray-100
       " to="#">Add Item</Link>
-                            </li>
-                            <li>
-                                <button className="
+                                </li>
+                                <li>
+                                    <button className="
         dropdown-item
         text-sm
         py-2
@@ -154,13 +155,15 @@ const Header = () => {
         text-gray-700
         hover:bg-gray-100
       " to="#">Logout</button>
-                            </li>
-                        </ul>
+                                </li>
+                            </ul>
+                        </div>
                     </div>
-                </div>
 
-            </div>
-        </nav>
+                </div>
+            </nav>
+            <Carousel></Carousel>
+        </div>
 
     );
 };
