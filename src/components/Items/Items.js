6577cd144a49550f6d6d622288b0000becc6597a
mahbub-webdev <mahabub2k1@gmail.com>
2022-05-17 11:03:57 +0600

    firebase setup

--- src/components/Items/Items.js
+++ src/components/Items/Items.js
@@ -4,7 +4,7 @@ import useItems from '../../hooks/useItem';
 import Item from '../Item/Item';
 
 const Items = () => {
-    const [items, setItems] = useItems();
+    const [items] = useItems();
     return (
         <>
             <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-4/5 mx-auto my-10 gap-5'>
